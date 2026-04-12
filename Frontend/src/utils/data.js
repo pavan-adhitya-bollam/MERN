7116f080f2c121364d74df8fc285cf678c84463a
2303a51047 <2303a51047@sru.edu.in>
2026-04-12 13:13:54 +0530

    feat: update frontend API endpoints to use deployed backend URL

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,5 +1,5 @@
-export const USER_API_ENDPOINT = "http://localhost:5001/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5001/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5001/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:5001/api/company";
-export const AUTH_API_ENDPOINT = "http://localhost:5001/api/auth";
+export const USER_API_ENDPOINT = "https://dreamhire-backend-ljay.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://dreamhire-backend-ljay.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://dreamhire-backend-ljay.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://dreamhire-backend-ljay.onrender.com/api/company";
+export const AUTH_API_ENDPOINT = "https://dreamhire-backend-ljay.onrender.com/api/auth";
